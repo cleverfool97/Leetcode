e1c6c06e10e719e08365b7717f3b8533a146036e
James Wang <jameswang97@ucla.edu>
2017-10-01 00:02:40 -0700

    Problem 237

---
+++ Questions/Questions/237 - deleteNodeInALinkedList.cpp
@@ -0,0 +1,34 @@
+#include "237 - deleteNodeInALinkedList.h"
+
+/*
+Difficulty: Easy
+Please refer to "237. Delete Node in a Linked List" for the problem statement.
+
+Solution:
+    This is slightly less traditional in that normally you are given a predecessor to the node to be deleted.
+    However, given the scenarios we will be using this function in, we can safely take the next node's value and place it in our current one, then delete the next node like normal.
+
+    Time Complexity: O(1)
+    Space Complexity: O(1)
+
+Be Aware:
+    -You can actually submit an answer that doesn't actually do any deletion and it will be accepted by LeetCode.
+    -Make sure that in practice you call delete; otherwise that memory isn't really being freed for further usage.
+*/
+
+/**
+* Definition for singly-linked list.
+* struct ListNode {
+*     int val;
+*     ListNode *next;
+*     ListNode(int x) : val(x), next(NULL) {}
+* };
+*/
+
+void Solution237::deleteNode(ListNode* node)
+{
+    node->val = node->next->val;
+    ListNode *temp = node->next;
+    node->next = node->next->next;
+    delete temp;
+}
--- Questions/Questions/237 - deleteNodeInALinkedList.h
+++ Questions/Questions/237 - deleteNodeInALinkedList.h
@@ -1,1 +1,16 @@
-#pragma once
+#ifndef LEET237_H
+#define LEET237_H
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution237
+{
+public:
+    void deleteNode(ListNode* node);
+};
+#endif
