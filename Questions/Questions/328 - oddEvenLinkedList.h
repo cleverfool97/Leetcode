8ea8baa876db4044dd5dccee4130d4469bf00455
James Wang <jameswang97@ucla.edu>
2017-10-01 01:23:48 -0700

    Fixed h file for Problem 328

--- Questions/Questions/328 - oddEvenLinkedList.h
+++ Questions/Questions/328 - oddEvenLinkedList.h
@@ -1,1 +1,24 @@
-#pragma once
+#ifndef LEET328_H
+#define LEET328_H
+
+/**
+* Definition for singly-linked list.
+* struct ListNode {
+*     int val;
+*     ListNode *next;
+*     ListNode(int x) : val(x), next(NULL) {}
+* };
+*/
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) { }
+};
+class Solution328
+{
+public:
+    ListNode* oddEvenList(ListNode* head);
+};
+#endif
