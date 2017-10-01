68abec62b578af34b3eb890fef8199ce2025cf82
James Wang <jameswang97@ucla.edu>
2017-10-01 01:47:38 -0700

    Removed extraneous comments

--- Questions/Questions/328 - oddEvenLinkedList.h
+++ Questions/Questions/328 - oddEvenLinkedList.h
@@ -1,15 +1,6 @@
 #ifndef LEET328_H
 #define LEET328_H
 
-/**
-* Definition for singly-linked list.
-* struct ListNode {
-*     int val;
-*     ListNode *next;
-*     ListNode(int x) : val(x), next(NULL) {}
-* };
-*/
-
 struct ListNode
 {
     int val;
