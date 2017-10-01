d37bbe6384e5a06461dc09658a9fd3b6b853c43d
James Wang <jameswang97@ucla.edu>
2017-10-01 12:08:20 -0700

    Problem 33

---
+++ Questions/Questions/033 - searchInRotatedSortedArray.cpp
@@ -0,0 +1,44 @@
+#include "033 - searchInRotatedSortedArray.h"
+#include "config.h"
+
+using namespace std;
+
+/*
+Difficulty: Medium
+Please refer to "33. Search in Rotated Sorted Array" for the problem statement.
+
+Solution:
+    Perform a binary search for the location that the array was rotated to, then treat the array as if it was a normal one by doing a normal binary search accounting for the offset.
+    We achieve this by finding the true middle in our actual search by adding the amount rotated mod by the size of the array to get the midpoint as if the array started from the offset.
+
+    Let N be the size of the input array
+    Time Complexity: O(logN), two binary searches
+    Space Complexity: O(1), solution performed iteratively and not recursively with no additional data structures
+*/
+
+int search(vector<int>& nums, int target)
+{
+    int low = 0;
+    int high = nums.size() - 1;
+    while (low < high)
+    {
+        int mid = low + (high - low) / 2;
+        if (nums[mid] > nums[high]) low = mid + 1;
+        else high = mid;
+    }
+
+    int rotation = low;
+    low = 0;
+    high = nums.size() - 1;
+
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        int mid_rotated = (mid + rotation) % nums.size();
+        if (nums[mid_rotated] == target) return mid_rotated;
+        if (nums[mid_rotated] < target) low = mid + 1;
+        else high = mid - 1;
+    }
+
+    return -1;
+}
---
+++ Questions/Questions/033 - searchInRotatedSortedArray.h
@@ -0,0 +1,13 @@
+#ifndef LEET033_H
+#define LEET033_H
+
+#include "config.h"
+
+class Solution033
+{
+public:
+    int search(std::vector<int>& nums, int target);
+};
+
+#endif
+
